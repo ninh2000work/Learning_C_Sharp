91fb5f318a6eb562d94d095312862eb81f2beffb
ninh2000work <ninh2000.work@gmail.com>
2026-04-06 14:02:59 +0700

    260406_v0

---
+++ Bai1_NhapXuatCoBanTrongCSharp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_NhapXuatCoBanTrongCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region CauTrucCobanLenhNhapXuat
+            Console.Write("HelloWorld\n");
+            Console.WriteLine(1);
+            Console.Write(2.3f);
+            Console.Write(Environment.NewLine);
+            Console.WriteLine(Console.ReadLine());
+            Console.ReadKey();
+            #endregion
+
+            #region Console.Write()
+            int a = 1;
+            Console.Write("a = ");
+            Console.Write(a);
+            Console.Write(Environment.NewLine);
+            Console.Write("a = " + a);
+            Console.Write(Environment.NewLine);
+            Console.Write("a = {0}", a);
+            Console.Write(Environment.NewLine);
+            Console.ReadKey();
+            #endregion
+
+            #region Console.WriteLine()
+            int b = 2;
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("b = {0}", b);
+            Console.ReadKey();
+            #endregion
+
+            #region Console.Read()
+            Console.WriteLine(Console.Read());
+            Console.ReadKey();
+            #endregion
+
+            #region Console.ReadLine()
+            Console.WriteLine(Console.ReadLine());
+            Console.ReadKey();
+            #endregion
+
+            #region Console.ReadKey()
+            Console.ReadKey();
+            Console.ReadKey(false);
+            Console.ReadKey(true);
+            #endregion
+        }
+    }
+}
---
+++ Bai1_NhapXuatCoBanTrongCSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bai1_NhapXuatCoBanTrongCSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bai1_NhapXuatCoBanTrongCSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d361a68a-dc92-4439-9358-f80240f2b01f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
