b7069427a57b6f24e60d34ee635d46ebb5b2dd12
ninh2000work <ninh2000.work@gmail.com>
2026-04-07 13:04:31 +0700

    260407_v1

---
+++ Bai9_KieuDuLieuObjectTrongCSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai9_KieuDuLieuObjectTrongCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Object
+            //object obj1 = 10;
+            //object obj2 = 10;
+            //object obj3 = 20;
+
+            //string a = obj1.ToString();
+            //Console.WriteLine(a);
+
+            //Type b = obj1.GetType();
+            //Console.WriteLine(b);
+
+            //bool c = obj1.Equals(obj3);
+            //Console.WriteLine(c);
+
+            //int d = obj1.GetHashCode();
+            //Console.WriteLine(d);
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Boxing
+            #endregion
+
+            #region Unboxing
+            #endregion
+
+            #region Var
+            #endregion
+        }
+    }
+}
