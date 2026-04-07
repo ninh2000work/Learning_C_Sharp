84a969f7da4e594ef53695f6ef6fe3cf528bf194
ninh2000work <ninh2000.work@gmail.com>
2026-04-07 16:00:55 +0700

    260407_v2

--- Bai9_KieuDuLieuObjectTrongCSharp/Program.cs
+++ Bai9_KieuDuLieuObjectTrongCSharp/Program.cs
@@ -31,12 +31,24 @@ namespace Bai9_KieuDuLieuObjectTrongCSharp
             #endregion
 
             #region Boxing
+            //int a = 10;
+            //object b = a;
+            //Console.WriteLine(b);
+            //Console.ReadKey();
             #endregion
 
             #region Unboxing
+            //int a = 100;
+            //object b = a;
+            //int c = (int)b;
+            //Console.WriteLine(c);
+            //Console.ReadKey();
             #endregion
 
             #region Var
+            //var a = 10;
+            //Console.WriteLine(a);
+            //Console.ReadKey();
             #endregion
         }
     }
