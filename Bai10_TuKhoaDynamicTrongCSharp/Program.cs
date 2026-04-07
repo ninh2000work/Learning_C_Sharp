121d3ea2d09ed2fb524276e3e7fc7402033fd490
ninh2000work <ninh2000.work@gmail.com>
2026-04-07 16:52:33 +0700

    260407_v2

---
+++ Bai10_TuKhoaDynamicTrongCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai10_TuKhoaDynamicTrongCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dynamic
+            dynamic a = 10;
+            Console.WriteLine(a);
+            Console.ReadKey();
+
+            string b = "HelloWorld";
+            dynamic c = b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
---
+++ Bai11_CauTrucLapGotoTrongCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai11_CauTrucLapGotoTrongCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu:
+            Console.WriteLine("HelloWorld");
+            Console.ReadKey();
+            goto Menu;
+        }
+    }
+}
