79182f1549211b2d9661bc2e7d6c481708adad91
ninh2000work <ninh2000.work@gmail.com>
2026-04-07 10:45:09 +0700

    260407_v0

--- Bai7_IfElseTrongCSharp/Program.cs
+++ Bai7_IfElseTrongCSharp/Program.cs
@@ -10,7 +10,42 @@ namespace Bai7_IfElseTrongCSharp
     {
         static void Main(string[] args)
         {
-            #region
+            #region If
+            int a = 1;
+            if (a == 1)
+            {
+                Console.WriteLine("a = 1");
+            }
+            Console.ReadKey();
+            #endregion
+
+            #region If Else
+            int b = 3;
+            if (b == 2)
+                Console.WriteLine("b = 2");
+            else
+            {
+                Console.WriteLine("b != 2");
+                Console.WriteLine("b = " + b);
+            }
+            Console.ReadKey();
+            #endregion
+
+            #region If Elseif Else
+            int c = 4;
+            if (c == 3)
+                Console.WriteLine("c = 3");
+            else if (c < 3)
+            {
+                Console.WriteLine("c < 3");
+                Console.WriteLine("c = " + c);
+            }
+            else
+            {
+                Console.WriteLine("c > 3");
+                Console.WriteLine("c = " + c);
+            }
+            Console.ReadKey();
             #endregion
         }
     }
