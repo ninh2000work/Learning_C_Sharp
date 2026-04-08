a4af6095ee6258207f8f215b4f7cc2df41e8858c
ninh2000work <ninh2000.work@gmail.com>
2026-04-08 23:40:17 +0700

    260408_v2

--- Bai18_MangMotChieuTrongCSharp/Program.cs
+++ Bai18_MangMotChieuTrongCSharp/Program.cs
@@ -10,6 +10,26 @@ namespace Bai18_MangMotChieuTrongCSharp
     {
         static void Main(string[] args)
         {
+            int[] MangInt = new int[10];   //Khai báo và cấp phát vùng nhớ cho mảng
+            MangInt[0] = 1;
+            MangInt[1] = 5;
+            MangInt[2] = 10;
+            string[] MangChuoi = new string[] { "Hello", "World", "HelloWorld" };     //Khai báo, cấp phát vùng nhớ và khởi tạo giá trị cho mảng
+            double[] MangDouble = { 5.6, 1.8, 3.7 };    //Khởi tạo giá trị cho mảng
+
+            int a = MangInt.Length;
+            Console.WriteLine("MangInt.Length = " + a);
+
+            long b = MangChuoi.LongLength;
+            Console.WriteLine("MangChuoi.LongLength = " + b);
+
+            int c = MangInt.GetLength(0);
+            Console.WriteLine("MangInt.GetLength(0) = " + c);
+
+            long d = MangChuoi.GetLongLength(0);
+            Console.WriteLine("MangChuoi.GetLongLength(0) = " + d);
+
+            Console.ReadKey();
         }
     }
 }
