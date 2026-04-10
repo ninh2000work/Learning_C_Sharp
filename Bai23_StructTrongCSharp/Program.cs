1b7126e4f12c9ea37fdee23f72d6da313b516076
ninh2000work <ninh2000.work@gmail.com>
2026-04-10 23:49:05 +0700

    260410_v1

--- Bai23_StructTrongCSharp/Program.cs
+++ Bai23_StructTrongCSharp/Program.cs
@@ -8,8 +8,43 @@ namespace Bai23_StructTrongCSharp
 {
     internal class Program
     {
+        struct NhanVien
+        {
+            public String FullName;
+            public int ID;
+            public string PhoneNumber;
+            public double Salary;
+        }
+
+        static void NhapThongTin(out NhanVien NV)
+        {
+            Console.Write("Full Name: ");
+            NV.FullName = Console.ReadLine();
+            Console.Write("ID: ");
+            NV.ID = int.Parse(Console.ReadLine());
+            Console.Write("Phone Number: ");
+            NV.PhoneNumber = Console.ReadLine();
+            Console.Write("Salary (milion VND): ");
+            NV.Salary = Double.Parse(Console.ReadLine());
+        }
+
+        static void XuatThongTin(NhanVien NV)
+        {
+            Console.WriteLine("Full Name: " + NV.FullName);
+            Console.WriteLine("ID: " + NV.ID);
+            Console.WriteLine("Phone Number: " + NV.PhoneNumber);
+            Console.WriteLine("Salary (milion VND): " + NV.Salary);
+        }
+
         static void Main(string[] args)
         {
+            NhanVien A = new NhanVien();
+            Console.WriteLine("Nhap Thong Tin Nhan Vien:");
+            NhapThongTin(out A);
+            Console.WriteLine();
+            Console.WriteLine("Xuat Thong Tin Nhan Vien:");
+            XuatThongTin(A);
+            Console.ReadKey();
         }
     }
 }
--- Bai24_EnumTrongCSharp/Program.cs
+++ Bai24_EnumTrongCSharp/Program.cs
@@ -8,8 +8,59 @@ namespace Bai24_EnumTrongCSharp
 {
     internal class Program
     {
+        enum Color1
+        {
+            Red,
+            Green,
+            Blue,
+            Yellow
+        }
+        enum Color2
+        {
+            Red = 10,
+            Green = 20,
+            Blue = 30
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Nhap So Bat Ky: ");
+            int Choose = int.Parse(Console.ReadLine());
+
+            if (Choose == (int)Color1.Red)
+            {
+                Console.WriteLine("Mau Do 1");
+            }
+            else if (Choose == (int)Color1.Green)
+            {
+                Console.WriteLine("Mau Xanh La 1");
+            }
+            else if (Choose == (int)Color1.Blue)
+            {
+                Console.WriteLine("Mau Xanh Bien 1");
+            }
+            else if (Choose == (int)Color1.Yellow)
+            {
+                Console.WriteLine("Mau Vang 1");
+            }
+            else if (Choose == (int)Color2.Red)
+            {
+                Console.WriteLine("Mau Do 2");
+            }
+            else if (Choose == (int)Color2.Green)
+            {
+                Console.WriteLine("Mau Xanh La 2");
+            }
+            else if (Choose == (int)Color2.Blue)
+            {
+                Console.WriteLine("Mau Xanh Bien 2");
+            }
+            else
+            {
+                Console.WriteLine("Khong co mau nao thoa man!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
