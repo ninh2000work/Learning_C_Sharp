9fe29ace6e1ae1a74d3a02c207efe6fc63a1fc57
ninh2000work <ninh2000.work@gmail.com>
2026-04-20 22:02:17 +0700

    260420_v0

--- Bai43_CacLoaiPhamViTruyCapTrongOOP/Program.cs
+++ Bai43_CacLoaiPhamViTruyCapTrongOOP/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace Bai43_CacLoaiPhamViTruyCapTrongOOP
 {
+    class SinhVien
+    {
+        private string HoTen;
+        private int MSSV;
+
+        public string FullName
+        {
+            get
+            {
+                return HoTen;
+            }
+
+            set
+            {
+                HoTen = value;
+            }
+        }
+
+        public string GetFullName()
+        {
+            return HoTen;
+        }
+
+        public void SetID(int ID)
+        {
+            MSSV = ID;
+        }
+
+        public void InfoStudent()
+        {
+            Console.WriteLine("Fullname is {0}, ID is {1}", HoTen, MSSV);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            SinhVien A = new SinhVien();
+            A.FullName = "HelloWorld";
+            Console.WriteLine("FullName is " + A.FullName);
+
+            Console.WriteLine("FullName is " + A.GetFullName());
+            A.SetID(1234);
+            A.InfoStudent();
+
+            Console.ReadKey();
         }
     }
 }
