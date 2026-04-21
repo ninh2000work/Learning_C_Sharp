f874b9c21bd86dc703e4f7a8c3e782116d2aadb5
ninh2000work <ninh2000.work@gmail.com>
2026-04-21 17:15:49 +0700

    260421_v2

--- Bai46_DaHinhTrongOOP/Program.cs
+++ Bai46_DaHinhTrongOOP/Program.cs
@@ -8,8 +8,60 @@ namespace Bai46_DaHinhTrongOOP
 {
     internal class Program
     {
+        class People
+        {
+            public void Info1()
+            {
+                Console.WriteLine("People1");
+            }
+
+            public virtual void Info2()
+            {
+                Console.WriteLine("People2");
+            }
+        }
+
+        class Men : People
+        {
+            public void Info1()
+            {
+                Console.WriteLine("Men");
+            }
+        }
+
+        class Women : People
+        {
+            public override void Info2()
+            {
+                Console.WriteLine("Women");
+            }
+        }
+
+        abstract class University
+        {
+            public abstract void Info();
+        }
+
+        class School : University
+        {
+            public override void Info()
+            {
+                Console.WriteLine("School");
+            }
+        }
+
         static void Main(string[] args)
         {
+            People A = new Men();
+            A.Info1();
+
+            People B = new Women();
+            B.Info2();
+
+            University C = new School();
+            C.Info();
+
+            Console.ReadKey();
         }
     }
 }
